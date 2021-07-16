fdd7424ecba96a8952eaca6162ba239deb1702d6
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-16 11:44:17 +0300

    feedback vidget with statistics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
+
+import FeedbackVidget from './components/FeedbackVidget/FeedbackVidget';
+
+class App extends Component {
+  render() {
+    return <FeedbackVidget />;
+  }
+}
 
 export default App;
